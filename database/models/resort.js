e508298125c0080b3aefb4dff3e92f66a03a2fbb
2ssue <e2ssue@gmail.com>
2019-10-17 18:49:35 +0900

    New Feature: 조건쿼리에 맞는 resort를 반환하는 api 구현
    booking 테이블과 JOIN을 통해 해당 날짜에 예약이 가능한 숙소인지 확인할 수 있도록 하였음.
    booked가 해당 기간에 예약된 방의 개수를 계산해서 붙이면 가지고 있는 방의 개수와 비교해 resort를 띄울 예정

--- database/models/resort.js
+++ database/models/resort.js
@@ -1,4 +1,5 @@
 'use strict';
+const Sequelize = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   const Resort = sequelize.define('resort', {
     name: {
@@ -21,6 +22,48 @@ module.exports = (sequelize, DataTypes) => {
   Resort.getAllResort = async function(){
     return await Resort.findAll();
   }
-  
+
+  Resort.getPartResort = async function(params){
+    const [resortCondition, bookingCondition] = makeCondition(params);
+
+    return await Resort.findAll({
+      //'*'을 넣어서 하면 전체 컬럼에 대한 결과가 나오지 않아서 컬럼을 전부 입력하였음
+      attributes: ['name', 'photo_url', 'possible_guest', 'possible_room', 'price', [sequelize.fn('count', sequelize.col('bookings.resort_name')), 'booked']],
+      where: resortCondition,
+      include: [{
+        model: sequelize.models.booking,
+        attributes: [],
+        required: false, //false: LEFT OUTER JOIN, true: INNER JOIN
+        where: bookingCondition,
+      }],
+      group: ['name']
+    })
+  }
+
   return Resort;
 };
+
+const makeCondition = (params) => {
+  const { start, end, guest, price } = params;
+  let resortCondition = {};
+  let bookingCondition = {};
+
+  if(start && end){
+    bookingCondition = {
+      [Sequelize.Op.or]:{
+        start_date: { [Sequelize.Op.gte]: new Date(start.split('-'))},
+        end_date: { [Sequelize.Op.lte]: new Date(end.split('-'))}
+      }
+    };
+  }
+
+  if(guest){
+    resortCondition.possible_guest = { [Sequelize.Op.gte]: guest };
+  }
+
+  if(price){
+    resortCondition.price = { [Sequelize.Op.lte]: price };
+  }
+
+  return [resortCondition, bookingCondition];
+}
--- routes/resort.js
+++ routes/resort.js
@@ -6,7 +6,11 @@ const { verifyUser } = require('../auth/auth');
 router.use(verifyUser);
 
 router.get('/', async function(req, res, next){
-    res.json(await resort.getAllResort());
+    if(req.query){
+        res.json(await resort.getPartResort(req.query));
+    }else{
+        res.json(await resort.getAllResort());
+    }
 })
 
 module.exports = router;
