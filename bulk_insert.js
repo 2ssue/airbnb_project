5c08440d2b5ca308f0bb7c305be6e365c2f1db10
2ssue <e2ssue@gmail.com>
2019-10-17 22:11:21 +0900

    Update Feature: 파일 입력에 따른 에러처리 추가
    올바르지 않은 파일 형식, 존재하지 않은 파일, 파일을 입력하지 않는 등의 상황이 있을 수 있으므로 이에 대한 에러처리를 추가함

--- bulk_insert.js
+++ bulk_insert.js
@@ -1,28 +1,46 @@
 const parse = require('csv-parse');
 const filename = process.argv[2];
+
+if(!filename){
+    console.error('변환할 파일을 입력하세요');
+    return;
+}
+
 const fs = require('fs');
-const output = [];
-const csvData = fs.readFileSync(filename, 'utf8').trim();
+let csvData;
+try{
+    csvData = fs.readFileSync(filename, 'utf8').trim();
+}catch(err){
+    console.error(`error: 존재하지 않는 파일 [${filename}]`);
+    return;
+}
+
 const sequelize = require('./database/models').sequelize;
 
 sequelize.sync().then(() => {
     console.log('parsing...');
-    parse(csvData, {
-        trim: true,
-        cast: true,
-        columns: ['name','photo_url','possible_guest','possible_room','price'],
-    }).on('readable', function(){
-        let record
-        while(record = this.read()){
-            output.push(record);
-        }
-    }).on('end', function(){
-        sequelize.models.resort.bulkCreate(output).then(() => {
-            console.log('...complete');
-        }).catch(err => {
-            console.log(err);
-        }).finally(() => {
-            sequelize.close();
-        })
-    });
+    try{
+        const output = [];
+        parse(csvData, {
+            trim: true,
+            cast: true,
+            columns: ['name','photo_url','possible_guest','possible_room','price'],
+        }).on('readable', function(){
+            let record
+            while(record = this.read()){
+                output.push(record);
+            }
+        }).on('end', function(){
+            sequelize.models.resort.bulkCreate(output).then(() => {
+                console.log('...complete');
+            }).catch(err => {
+                console.log(err);
+            }).finally(() => {
+                sequelize.close();
+            })
+        });
+    }catch(err){
+        console.error('올바른 형식의 파일이 아닙니다');
+        process.exit();
+    }
 });
