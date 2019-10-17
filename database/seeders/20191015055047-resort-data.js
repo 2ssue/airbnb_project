b60713261369205bb2a5a0f7507d7dbe53628444
2ssue <e2ssue@gmail.com>
2019-10-17 21:55:45 +0900

    Chore: 테스트 seed 데이터 추가

--- database/seeders/20191015055047-resort-data.js
+++ database/seeders/20191015055047-resort-data.js
@@ -4,9 +4,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('resorts', [{
       name: 'resort1',
-      photo_url: 'https://www3.hilton.com/resources/media/hi/CZMPCHH/en_US/img/shared/full_page_image_gallery/main/HH_outpool01_7_1270x560_FitToBoxSmallDimension_Center.jpg',
+      photo_url: '',
       possible_guest: 5.0,
+      possible_room: 4,
       price: 100000
+    },{
+      name: 'resort2',
+      photo_url: '',
+      possible_guest: 3.1,
+      possible_room: 3,
+      price: 1000
+    },{
+      name: 'resort3',
+      photo_url: '',
+      possible_guest: 2.1,
+      possible_room: 2,
+      price: 200000
     }], {});
   },
 
---
+++ database/seeders/20191017054430-booking.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('bookings', [{
+      check_in: new Date([2019,10,17]),
+      check_out: new Date([2019,10,19]),
+      resort_name: 'resort3',
+      guest_id: 'user1'
+    },{
+      check_in: new Date([2019,10,16]),
+      check_out: new Date([2019,10,18]),
+      resort_name: 'resort3',
+      guest_id: 'user2'
+    },{
+      check_in: new Date([2019,10,17]),
+      check_out: new Date([2019,10,19]),
+      resort_name: 'resort3',
+      guest_id: 'user3'
+    },{
+      check_in: new Date([2019,10,17]),
+      check_out: new Date([2019,10,19]),
+      resort_name: 'resort1',
+      guest_id: 'user3'
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('bookings', null, {});
+  }
+};
