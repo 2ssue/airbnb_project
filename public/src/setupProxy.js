f20631fba78a30b8bc2a582f58c452998042d28d
2ssue <e2ssue@gmail.com>
2019-10-20 21:03:59 +0900

    Env: React 기존 서버와 연동
    npm-run-all 패키지를 이용해 동시에 실행될 수 있도록 함
    proxy를 설정해 서버에 요청을 보내고 받을 수 있도록 함

---
+++ public/src/setupProxy.js
@@ -0,0 +1,16 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    proxy('/users', {
+      target: 'http://localhost:3000',
+      changeOrigin: true
+    })
+  );
+  app.use(
+    proxy('/resorts', {
+      target: 'http://localhost:3000',
+      changeOrigin: true
+    })
+  );
+};
