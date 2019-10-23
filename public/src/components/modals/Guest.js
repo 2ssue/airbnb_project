39381e4ad30e54da0a9d35a65c423318d97c1973
2ssue <e2ssue@gmail.com>
2019-10-23 22:19:26 +0900

    Chore: 임시 Modal 파일 업로드

---
+++ public/src/components/modals/Guest.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Guest() {
+  return <div></div>;
+}
+
+export default Guest;
---
+++ public/src/components/modals/Price.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Price() {
+  return <div></div>;
+}
+
+export default Price;
